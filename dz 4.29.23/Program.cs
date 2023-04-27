6a53d13f3a71fcbeb49ae4ccc93b1901ec476356
VladosNasos <bezzzubkovlad@gmail.com>
2023-04-27 12:45:21 +0300

    Add project files.

---
+++ dz 4.29.23/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+public class KeyListener
+{
+    public event EventHandler EnterKeyPressed;
+    public event EventHandler SpaceKeyPressed;
+    public event EventHandler EscapeKeyPressed;
+    public event EventHandler F1KeyPressed;
+    public event EventHandler LeftKeyPressed;
+    public event EventHandler RightKeyPressed;
+    public event EventHandler UpKeyPressed;
+    public event EventHandler DownKeyPressed;
+    public class Person
+    {
+        public void Zitr(object sender, EventArgs e)
+        {
+            Console.WriteLine("Space key pressed. Zitr method called.");
+        }
+
+        public void Se1ect(object sender, EventArgs e)
+        {
+            Console.WriteLine("Enter key pressed. Se1ect method called.");
+        }
+
+        public void Move(object sender, EventArgs e)
+        {
+            Console.WriteLine("Arrow key pressed. Move method called.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyListener listener = new KeyListener();
+            Person person = new Person();
+
+            listener.SpaceKeyPressed += person.Zitr;
+            listener.EnterKeyPressed += person.Se1ect;
+            listener.LeftKeyPressed += person.Move;
+            listener.RightKeyPressed += person.Move;
+            listener.UpKeyPressed += person.Move;
+            listener.DownKeyPressed += person.Move;
+
+            listener.Listen();
+        }
+    }
+    public void Listen()
+    {
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.Enter:
+                    EnterKeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.Spacebar:
+                    SpaceKeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.Escape:
+                    EscapeKeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.F1:
+                    F1KeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    LeftKeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.RightArrow:
+                    RightKeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.UpArrow:
+                    UpKeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.DownArrow:
+                    DownKeyPressed?.Invoke(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}
---
+++ dz 4.29.23/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dz 4.29.23")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dz 4.29.23")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("386e8647-7361-43a8-8b14-da0ec8c857d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
